015fe89fdce9f82a498d43b2dc9e32acce28042b
zsozsoo13 <gidrozsoltika59@gmail.com>
2025-11-23 16:26:46 +0200

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // EZ A MÓDOSÍTOTT RÉSZ: a "domnus-aqua2" repository-névvel
-      base: '/domnus-aqua2/',
+      // EZ A KRITIKUS SOR a GitHub Pages-hez:
+      base: '/domnus-aqua2/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
